a572634f6f97cf86292ca26bda836457e84503d3
PMP <ptrkptz@gmail.com>
2019-03-11 21:24:48 -0500

    CSV download

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CSVLink, CSVDownload } from "react-csv";
 
 function getButtonText() {
-  return "Button Text";
+  return "Download Me!";
 }
 
 //  Create a react component
 const App = () => {
   return (
-    <div>
-      <label class="labelName" for="name">
-        Enter name:{" "}
-      </label>
-      <input id="name" type="text" />
-      <button style={{ backgroundColor: "blue", color: "white" }}>
+    <CSVLink data={data} headers={headers}>
+      <button style={{ backgroundColor: "red", color: "white" }}>
         {getButtonText()}
       </button>
-    </div>
+    </CSVLink>
   );
 };
+
+const headers = [
+  { label: "First Name", key: "firstname" },
+  { label: "Last Name", key: "lastname" },
+  { label: "Email", key: "email" }
+];
+
+const data = [
+  { firstname: "Ahmed", lastname: "Tomi", email: "ah@smthing.co.com" },
+  { firstname: "Raed", lastname: "Labes", email: "rl@smthing.co.com" },
+  { firstname: "Yezzi", lastname: "Min l3b", email: "ymin@cocococo.com" }
+];
 
 // Take the react component and show it on the screen
 ReactDOM.render(<App />, document.querySelector("#root"));
